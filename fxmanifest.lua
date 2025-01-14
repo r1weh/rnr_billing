fa456dc3d0cc5fde61aa18379992169dbcd5d148
r!wehh <118078284+r1weh@users.noreply.github.com>
2025-01-14 12:54:38 +0700

    [Update n Fixed] fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,3 @@
-shared_script '@dgc_acu/ai_module_fg-obfuscated.lua'
-shared_script '@dgc_acu/shared_fg-obfuscated.lua'
-shared_script '@klrp-aman/ai_module_fg-obfuscated.lua'
-shared_script '@klrp-aman/shared_fg-obfuscated.lua'
 fx_version 'adamant'
 game 'gta5'
 lua54 'yes'
@@ -12,20 +8,16 @@ shared_scripts {
 }
 
 client_scripts {
+    'bridge/client.lua',
     'client/*.lua',
 }
 
 server_scripts {
-    'server/*.lua',
     '@oxmysql/lib/MySQL.lua',
+    'bridge/server.lua',
+    'server/*.lua',
 }
 
 dependencies {
     'ox_lib'
 }
-client_script "@klrp-tnt/module/client_module.lua"
-shared_script "@klrp-tnt/module/server_module.lua"
-client_script "@klrp-tnt/module/client_module.lua"
-shared_script "@klrp-tnt/module/server_module.lua"
-client_script "@klrp-tnt/module/client_module.lua"
-shared_script "@klrp-tnt/module/server_module.lua"
